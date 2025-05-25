c4250b1479b887104488821eec6f6b4c170d3a8c
sultandaniels <sultan_daniels@berkeley.edu>
2025-05-25 09:22:15 -0700

    haystck lens

--- src/data_train.py
+++ src/data_train.py
@@ -2874,8 +2874,8 @@ if __name__ == '__main__':
             ckpt_pred_steps = gen_ckpt_pred_steps(model_name)
 
             # steps_in = [1,2,3,5,10]
-            steps_in = [1,2,3,7,8]
-            # steps_in = list(range(1,9))
+            # steps_in = [1,2,3,7,8]
+            steps_in = list(range(1,9))
 
             colors=['#000000', '#005CAB', '#E31B23', '#FFC325', '#00A651', '#9B59B6']
         
